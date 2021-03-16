22176bf527db4380d64c1af6178cd19a181449ce
khang-tran <58164790+khang-tran-upheads@users.noreply.github.com>
2021-03-16 15:07:47 +0700

    add product entity

---
+++ eCommerce.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Domain.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Domain.Entities
+{
+    public class Product : Entity, IAuditable
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public ProductCategory Category { get; set; }
+
+        public Guid OwnerId { get; set; }
+        public User Owner { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public string LastUpdatedBy { get; set; }
+    }
+}
---
+++ eCommerce.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using eCommerce.Domain.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Domain.Entities
+{
+    public class ProductCategory : Entity
+    {
+        public string Name { get; set; }
+    }
+}
