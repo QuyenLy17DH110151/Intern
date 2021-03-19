6c3907b381ab7da6ecadfe8288265f67ed4153c8
QUYEN LY <quyen.ly.151@outlook.com>
2021-03-19 11:41:56 +0700

    Search Inventory API

---
+++ eCommerce.Application/Services/Inventories/InventoryReturnModels.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using eCommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Application.Services.Inventories
+{
+   public class InventoryReturnModels
+   {
+        public class Inventory
+        {
+            public Guid Id { get; set; }
+            public Guid ProductId { get; set; }
+            public Product Product { get; set; }
+            public int Quantity { get; set; }
+        }
+        public class MappingProfile : Profile
+        {
+            public MappingProfile()
+            {
+                CreateMap<Domain.Entities.Inventory, Inventory>();
+            }
+        }
+    }
+}
---
+++ eCommerce.Persistence/Repositories/InventoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using eCommerce.Domain.Entities;
+using eCommerce.Domain.Repositories;
+using eCommerce.Domain.Repositories.Models;
+using eCommerce.Domain.Seedwork;
+using eCommerce.Domain.Shared.Models;
+using eCommerce.Persistence.QueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Persistence.Repositories
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly GenericRepository<Inventory> _genericRepo;
+
+        public IUnitOfWork UnitOfWork => _dbContext;
+
+        public InventoryRepository(ApplicationDbContext context)
+        {
+            _dbContext = context;
+            _genericRepo = new GenericRepository<Inventory>(_dbContext.Set<Inventory>());
+        }
+
+        public async Task<PaginatedResult<Inventory>> SearchAsync(SearchInventoryModel rq)
+        {
+            // filter
+            var queryObject = QueryObject<Inventory>.Empty;
+
+            if (!string.IsNullOrWhiteSpace(rq.Keyword))
+            {
+                var keyword = rq.Keyword;
+                queryObject.And(new InventoryQueryObjects.ContainsKeyword(keyword));
+            }
+
+            // orderby
+            if (!rq.Sort.Any())
+            {
+                rq.Sort.Add(new SortItem { FieldName = nameof(User.IdentityKey) });
+            }
+
+            rq.Sort.ForEach(x => queryObject.AddOrderBy(x.FieldName, x.IsDescending));
+
+
+            // execute
+            var result = await _genericRepo.SearchAsync(queryObject, rq.Pagination);
+            return result;
+        }
+    }
+}
