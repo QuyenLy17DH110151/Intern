d7566d71a32fe87c760318baa7198928b579e588
Nguyễn Văn Dũng <55054527+nvdung-khtn@users.noreply.github.com>
2021-04-06 14:28:20 +0700

    update_db_byNam

--- eCommerce.Domain/Entities/KeyResetPassword.cs
+++ eCommerce.Domain/Entities/KeyResetPassword.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using eCommerce.Domain.Seedwork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace eCommerce.Domain.Entities
 {
-    public class KeyResetPassword
+    public class KeyResetPassword : Entity
     {
         public string KeyParam { get; set; }
         public Guid UserId { get; set; }
