53308f2145e166fd98f42117bbfd024c4df65bb5
Nam Nguyen <nam030319991@gmail.com>
2021-04-06 14:23:00 +0700

    add model keyResetPassword

---
+++ eCommerce.Domain/Entities/KeyResetPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Domain.Entities
+{
+    public class KeyResetPassword
+    {
+        public string KeyParam { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
