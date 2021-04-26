c1d587c29fd468e74d55623350e5e8b871ffea99
Nam Nguyen <nam030319991@gmail.com>
2021-04-26 14:08:46 +0700

    check permission update Inventory

--- eCommerce.Application/Services/Inventories/InventoryService.cs
+++ eCommerce.Application/Services/Inventories/InventoryService.cs
@@ -52,7 +52,17 @@ namespace eCommerce.Application.Services.Inventories
             if (inventory == null)
             {
                 throw new EntityNotFound("inventory");
-            }         
+            }
+
+            //check permission of user with inventory 
+            string username = _applicationContext.Principal.Username;
+            var product = await _productRepository.GetProductByIdAsync(inventory.ProductId);
+            var owner = await _userRepository.GetByIdAsync(product.OwnerId);
+            if (owner.Username != username && _applicationContext.Principal.Role == UserRoles.Seller)
+            {
+                throw new BusinessException("User not permission");
+            }
+
 
             //set rowVersion and quantity to request
             inventory.Quantity = rq.Quantity;
