9e2ffae12e8464723dacce564918826603037efe
QUYEN LY <quyen.ly.151@outlook.com>
2021-04-28 14:46:33 +0700

    Search_products by categoty , price , name

--- Frontend/eCommerce.FrontStore/ClientApp/src/app/shared/components/search-bar/search-bar.component.ts
+++ Frontend/eCommerce.FrontStore/ClientApp/src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,7 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-import { NavService, Menu } from "../../services/nav.service";
 import { Router } from "@angular/router";
-import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "header-search-bar",
@@ -9,16 +7,10 @@ import { FormBuilder, FormGroup } from "@angular/forms";
   styleUrls: ["./search-bar.component.scss"],
 })
 export class SearchBarComponent implements OnInit {
-  searchForm: FormGroup;
-
   @ViewChild("searchTerm") searchTerm: ElementRef;
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.searchForm = this.fb.group({
-      term: "",
-    });
-  }
+  ngOnInit(): void {}
 
   // Click Toggle menu (Mobile)
   toggletNavActive(item) {
@@ -33,12 +25,5 @@ export class SearchBarComponent implements OnInit {
   }
   onKeydown() {
     this.onClick();
-    // let input = (document.getElementById("searchTerm") as HTMLInputElement)
-    //   .value;
-    // this.router.navigate([
-    //   "/shop/collection/left/sidebar",
-    //   { searchTerm: input },
-    // ]);
-    // (document.getElementById("searchTerm") as HTMLInputElement).value = "";
   }
 }
