d7b74ca275fb4dc8979210af69265eba0d9a795e
Nam Nguyen <nam030319991@gmail.com>
2021-05-04 16:35:43 +0700

    Add product rating

---
+++ eCommerce.Domain/Entities/ProductRating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Domain.Entities
+{
+    public class ProductRating
+    {
+        public string FullName { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public string Email { get; set; }
+
+        public string ReviewTitle { get; set; }
+
+        public string ReviewContent { get; set; }
+    }
+}
