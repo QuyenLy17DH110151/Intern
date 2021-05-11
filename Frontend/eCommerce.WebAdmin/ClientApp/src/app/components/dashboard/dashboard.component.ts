f98ea603c8f3fa6a1c790349947eabc675c5d0f6
QUYEN LY <quyen.ly.151@outlook.com>
2021-05-11 14:11:34 +0700

    add HightChart to FE

--- Frontend/eCommerce.WebAdmin/ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/eCommerce.WebAdmin/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { doughnutData, pieData } from '../../shared/data/chart';
 import { DashboardClient } from 'src/app/api-clients/Dashboard.client';
 import { SearchRequestOrder } from 'src/app/api-clients/models/order.model';
 import { OrderViewModel } from '../orders/order.viewModel';
+import * as HighCharts from 'highcharts';
 
 @Component({
     selector: 'app-dashboard',
@@ -36,6 +37,8 @@ export class DashboardComponent implements OnInit {
         this.getCountUser();
         this.getCountProduct();
         this.getLastedOrder();
+        this.pieChartBrowser();
+        // this.createChart();
         // this._dashBoard.getSumEarnings();
     }
 
@@ -84,6 +87,80 @@ export class DashboardComponent implements OnInit {
             this.orderListVM = this.orders.map(
                 (order, index) => new OrderViewModel(order, index)
             );
+        });
+    }
+
+    pieChartBrowser() {
+        HighCharts.chart('pieChart', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',
+            },
+            title: {
+                text: 'Best Selling Categories',
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    },
+                },
+            },
+            series: [
+                {
+                    name: 'Brands',
+                    colorByPoint: true,
+                    type: undefined,
+                    data: [
+                        {
+                            name: 'Chrome',
+                            y: 61.41,
+                            sliced: true,
+                            selected: true,
+                        },
+                        {
+                            name: 'Internet Explorer',
+                            y: 11.84,
+                        },
+                        {
+                            name: 'Firefox',
+                            y: 10.85,
+                        },
+                        {
+                            name: 'Edge',
+                            y: 4.67,
+                        },
+                        {
+                            name: 'Safari',
+                            y: 4.18,
+                        },
+                        {
+                            name: 'Sogou Explorer',
+                            y: 1.64,
+                        },
+                        {
+                            name: 'Opera',
+                            y: 1.6,
+                        },
+                        {
+                            name: 'QQ',
+                            y: 1.2,
+                        },
+                        {
+                            name: 'Other',
+                            y: 2.61,
+                        },
+                    ],
+                },
+            ],
         });
     }
 }
